27bd9e281b03da1337d5b8af128ef478519eb308
Markus Bilz <github@markusbilz.com>
2023-07-24 10:00:00 +0200

    replace setup.py by pyproject.toml

--- tclf/tests/test_common.py
+++ tclf/tests/test_common.py
@@ -1,15 +1,11 @@
 import pytest
-
 from sklearn.utils.estimator_checks import check_estimator
 
-from tclf import TemplateEstimator
-from tclf import TemplateClassifier
-from tclf import TemplateTransformer
+from tclf import TemplateClassifier, TemplateEstimator, TemplateTransformer
 
 
 @pytest.mark.parametrize(
-    "estimator",
-    [TemplateEstimator(), TemplateTransformer(), TemplateClassifier()]
+    "estimator", [TemplateEstimator(), TemplateTransformer(), TemplateClassifier()]
 )
 def test_all_estimators(estimator):
     return check_estimator(estimator)
